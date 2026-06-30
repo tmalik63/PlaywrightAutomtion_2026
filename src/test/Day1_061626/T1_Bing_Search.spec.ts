7ec2031b0e2d74dc75a95f8988edf44cd3b96e4a
tmalik63 <tahasabirmalik@gmail.com>
2026-06-29 20:47:53 -0400

    first automation project

---
+++ src/test/Day1_061626/T1_Bing_Search.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('bing search test', async ({ page }) => {
+    await page.goto('https://www.bing.com/');
+await page.locator("//*[@name='q']").fill('helloworld');
+await page.locator("//*[@name='q']").press('Enter');
+await page.waitForTimeout(5000);
+let searchResults = await page.locator("//*[@class='sb_count']").textContent();
+let arrayResults = searchResults?.split(' ');
+console.log("Total Search Results: " + arrayResults?.[1]);
+
+
+});
--- src/test/Day2_062226/T1_USPS_MouseHover.spec.ts
+++ src/test/Day2_062226/T1_USPS_MouseHover.spec.ts
@@ -1,18 +1,8 @@
-import test from "playwright/test";
+import test from "@playwright/test";
 
-test ("USPS mouse hover test", async ({ page }) => {
+test("USPS Mouse Hover Test", async ({ page }) => {
 
-    //go to usps
-    await page.goto("https://www.usps.com/");
 
-    //hover over quick tools
-    await page.locator("//*[text()='Quick Tools']").hover();
-
-    await page.waitForTimeout(2000);
-
-    //click on track a package
-    await page.locator("//*[text()='Track a Package']").click();
-
-    await page.waitForTimeout(2000);
-
+    
+  
 });
